c10740144c453e267d8179955506b6449a4d2c4e
Elwyn Harazaki <Harazaki@MGXA2EHA.local>
2017-12-05 01:48:13 +0700

    Updated 05.12.2017

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 import Header from './src/components/Header';
 import NewsFeed from './src/components/NewsFeed';
@@ -7,8 +7,13 @@ import NewsFeed from './src/components/NewsFeed';
 const App = () => {
 	return (
 		<View style={{ flex: 1, backgroundColor: '#F3F1F6' }}>
-			<Header />
-				<NewsFeed />
+         <StatusBar
+            barStyle='default'
+            backgroundColor='transparent'
+            translucent
+         />
+         <Header />
+			<NewsFeed />
 		</View>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,16 +14,13 @@ const Header = (props) => {
 const styles = {
    headerStyle: {
       alignItems: 'center',
-      backgroundColor: '#3E206B',
-      elevation: 2,
-      shadowColor: '#083861',
-      shadowOpacity: 0.9
+      backgroundColor: 'transparent',
    },
    
    textStyle: {
       color: '#F9F9F9',
       fontSize: 25,
-      padding: 5
+      padding: 8
    }
 };
 
